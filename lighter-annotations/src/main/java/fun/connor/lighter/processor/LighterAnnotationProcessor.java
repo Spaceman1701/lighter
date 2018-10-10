e2b24a09a643faa39d1790961753127f236a2fff
Ethan Hunter <fc.spaceman@gmail.com>
2018-10-10 16:48:38 -0600

    uhg

---
+++ lighter-annotations/src/main/java/fun/connor/lighter/processor/LighterAnnotationProcessor.java
@@ -0,0 +1,41 @@
+package fun.connor.lighter.processor;
+
+import fun.connor.lighter.declarative.*;
+import javafx.geometry.Pos;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes({"fun.connor.lighter.declarative.ResourceController"})
+public class LighterAnnotationProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "this is a test error");
+        return true;
+    }
+
+//    @Override
+//    public Set<String> getSupportedAnnotationTypes() {
+//        Set<String> annotations = new HashSet<>();
+//        annotations.add(Body.class.getCanonicalName());
+//        annotations.add(Delete.class.getCanonicalName());
+//        annotations.add(Get.class.getCanonicalName());
+//        annotations.add(Post.class.getCanonicalName());
+//        annotations.add(Put.class.getCanonicalName());
+//        annotations.add(QueryParams.class.getCanonicalName());
+//        annotations.add(ResourceController.class.getCanonicalName());
+//        return annotations;
+//    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+}
