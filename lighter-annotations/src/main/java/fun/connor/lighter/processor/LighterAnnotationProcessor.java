7db64ae2a8c6edec0d27b42b56aff1be643eaa0d
Ethan Hunter <fc.spaceman@gmail.com>
2018-10-15 13:36:45 -0600

    just give up and use autoservice

--- lighter-annotations/src/main/java/fun/connor/lighter/processor/LighterAnnotationProcessor.java
+++ lighter-annotations/src/main/java/fun/connor/lighter/processor/LighterAnnotationProcessor.java
@@ -1,11 +1,11 @@
 package fun.connor.lighter.processor;
 
+import com.google.auto.service.AutoService;
 import fun.connor.lighter.declarative.*;
-import javafx.geometry.Pos;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
@@ -13,7 +13,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-@SupportedAnnotationTypes({"fun.connor.lighter.declarative.ResourceController"})
+@AutoService(Processor.class)
 public class LighterAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -21,18 +21,18 @@ public class LighterAnnotationProcessor extends AbstractProcessor {
         return true;
     }
 
-//    @Override
-//    public Set<String> getSupportedAnnotationTypes() {
-//        Set<String> annotations = new HashSet<>();
-//        annotations.add(Body.class.getCanonicalName());
-//        annotations.add(Delete.class.getCanonicalName());
-//        annotations.add(Get.class.getCanonicalName());
-//        annotations.add(Post.class.getCanonicalName());
-//        annotations.add(Put.class.getCanonicalName());
-//        annotations.add(QueryParams.class.getCanonicalName());
-//        annotations.add(ResourceController.class.getCanonicalName());
-//        return annotations;
-//    }
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new HashSet<>();
+        annotations.add(Body.class.getCanonicalName());
+        annotations.add(Delete.class.getCanonicalName());
+        annotations.add(Get.class.getCanonicalName());
+        annotations.add(Post.class.getCanonicalName());
+        annotations.add(Put.class.getCanonicalName());
+        annotations.add(QueryParams.class.getCanonicalName());
+        annotations.add(ResourceController.class.getCanonicalName());
+        return annotations;
+    }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
