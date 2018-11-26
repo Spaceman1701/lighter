6e26a95640e4941051dc8aee11b04d7d1ae1592a
Ethan Hunter <fc.spaceman@gmail.com>
2018-11-26 12:22:27 -0700

    make all Lighter annotations documented

--- lighter-core/src/main/java/fun/connor/lighter/declarative/QueryParams.java
+++ lighter-core/src/main/java/fun/connor/lighter/declarative/QueryParams.java
@@ -1,10 +1,8 @@
 package fun.connor.lighter.declarative;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface QueryParams {
