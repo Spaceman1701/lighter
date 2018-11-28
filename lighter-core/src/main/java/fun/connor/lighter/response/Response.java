7951c030c129728b7baeefc9061d96f18b97b13f
Ethan Hunter <fc.spaceman@gmail.com>
2018-11-28 02:18:45 -0700

    more javadoc

--- lighter-core/src/main/java/fun/connor/lighter/response/Response.java
+++ lighter-core/src/main/java/fun/connor/lighter/response/Response.java
@@ -3,11 +3,16 @@ package fun.connor.lighter.response;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * HTTP Response. All Lighter endpoint methods must return an instance of
+ * this class. This class is immutable.
+ * @param <T> The type of the response body. If the response has no body, this should be {@link Void}
+ */
 public class Response<T> {
 
-    private T content;
-    private int status;
-    private Map<String, String> headers;
+    private final T content;
+    private final int status;
+    private final Map<String, String> headers;
 
     private Response(ResponseState<T> state) {
         this.content = state.getContent();
@@ -15,26 +20,54 @@ public class Response<T> {
         this.headers = state.getHeaders();
     }
 
+    /**
+     * Create a new response with no headers and no response body. The response status code will
+     * be set to 200 - OK.
+     * @return the new response
+     */
     public static Response<Void> create() {
         return new Response<>(new ResponseState<>(null, 200, new HashMap<>()));
     }
 
+    /**
+     * Apply a decorator transformation to this response. The response decorator applies some
+     * transformation to the response. The result of the transformation is returned. Examples of this are
+     * changing the status, adding a header, or adding a response body
+     * <p>
+     *     This is intended to be used a fluent method.
+     * </p>
+     * @param transformer The transformation to apply
+     * @param <R> the type of the response body after the transformation is applied
+     * @return a response with the applied transformation
+     */
     public <R> Response<R> with(ResponseDecorator<T, R> transformer) {
         return new Response<>(transformer.apply(new ResponseState<>(content, status, headers)));
     }
 
+    /**
+     * @return @[code true} iff the response has a body
+     */
     public boolean hasContent() {
         return content != null;
     }
 
+    /**
+     * @return the response body or {@code null} if it does not have one
+     */
     public T getContent() {
         return content;
     }
 
+    /**
+     * @return the HTTP status code of this response.
+     */
     public int getStatus() {
         return status;
     }
 
+    /**
+     * @return this response's HTTP headers.
+     */
     public Map<String, String> getHeaders() {
         return headers;
     }
--- lighter-core/src/main/java/fun/connor/lighter/response/Responses.java
+++ lighter-core/src/main/java/fun/connor/lighter/response/Responses.java
@@ -3,26 +3,54 @@ package fun.connor.lighter.response;
 import fun.connor.lighter.http.HttpHeaders;
 import fun.connor.lighter.http.StatusCodes;
 
+/**
+ * Utility class for building common HTTP response types.
+ */
 public final class Responses {
 
     private Responses() {}
 
 
+    /**
+     * Create status 200 - OK response with the Content-Type set to application/json.
+     * @param content the response body
+     * @param <T> the type of teh response body
+     * @return a new response
+     */
     public static <T> Response<T> json(T content) {
         return json(content, StatusCodes.OK);
     }
 
+    /**
+     * Create a Content-Type application/json response with a provided status code
+     * @param content the response body
+     * @param status the response status
+     * @param <T> the type of the response body
+     * @return a new response
+     */
     public static <T> Response<T> json(T content, int status) {
         return Response.create()
                 .with(JsonContent.from(content))
                 .with(StatusResponse.from(status));
     }
 
+    /**
+     * Create a response with no body and status 202 - ACCEPTED.
+     * @return a new response
+     */
     public static Response<Void> accepted() {
         return Response.create()
                 .with(StatusResponse.from(StatusCodes.ACCEPTED));
     }
 
+    /**
+     * Create a redirect response to redirect browsers or other clients. This response
+     * has no body. If provided status is not a 3xx redirect code, this method throws
+     * an {@link IllegalArgumentException}
+     * @param status a valid redirect status code
+     * @param url the url to redirect to
+     * @return the response
+     */
     public static Response<Void> redirect(int status, String url) {
         if (!StatusCodes.isRedirect(status)) {
             throw new IllegalArgumentException(status + " is not a valid redirect status code");
@@ -32,6 +60,13 @@ public final class Responses {
                 .with(StatusResponse.from(status));
     }
 
+    /**
+     * Return a response with no content but with a content type. This is useful when some
+     * conditions of an endpoint method return a response with content and some do not.
+     * @param status the response status
+     * @param <T> the type of the response body
+     * @return a new response
+     */
     public static <T> Response<T> noContent(int status) {
         return Response.create()
                 .with(StatusResponse.from(status))
